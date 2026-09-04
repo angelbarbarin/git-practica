2a11e4b4a454d44111c56bf2cc73640571efba6e
angelbarbarin <angelibarbarin@gmail.com>
2026-09-04 15:41:41 -0600

    Muestra la fecha y hora de ejecución en app.js

--- app.js
+++ app.js
@@ -2,3 +2,7 @@
 // Tecnologías de Desarrollo en el Servidor
 
 console.log("Hola, este es mi primer proyecto con Git y Node.js");
+
+// Muestra la fecha y hora en que se ejecuta el programa
+const fecha = new Date();
+console.log("Ejecutado el: " + fecha.toLocaleString());
